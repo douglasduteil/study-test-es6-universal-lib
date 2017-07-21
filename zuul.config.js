4c5ae16e936dc6d4bd1ef138dfb58dd4f4cc2637
Douglas Duteil <douglasduteil@gmail.com>
2017-07-21 11:05:01 +0200

    chore(pkg): update zuul config

---
+++ zuul.config.js
@@ -0,0 +1,29 @@
+//
+
+
+const defaultConfig = {
+  name: 'study-test-es6-universal-lib',
+  ui: 'tape'
+};
+
+const commitSha1 = (process.env.TRAVIS_COMMIT || '').slice(0, 7);
+const travisConfig = {
+  browsers: [
+    {
+      name: process.env.BROWSER_NAME,
+      version: process.env.BROWSER_VERSION
+    }
+  ],
+  html: 'test/basic.html',
+  name: [
+    process.env.TRAVIS_BRANCH,
+    '(' + commitSha1 + ')',
+    '-',
+    process.env.TRAVIS_JOB_NUMBER,
+  ].join(' '),
+  ui: 'tape'
+};
+
+module.exports = false ||
+  (process.env.TRAVIS && travisConfig) ||
+  defaultConfig;
